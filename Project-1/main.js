71fd121525d4aafa38f81846dce7c034960dda8e
NotActuallyyHectorr <144372122+NotActuallyyHectorr@users.noreply.github.com>
2023-10-07 19:20:44 -0500

    Add files via upload

---
+++ Project-1/main.js
@@ -0,0 +1,9 @@
+alert('Welcome To Incognito Website');
+alert('SHHHHHHH');
+
+var welcome = 'Hello';
+var website = 'This is a top secrete Website, ';
+var intro = website + welcome + 'Dont spread the word.';
+var introEL = document.getElementById('intro');
+
+introEL.textContent = intro;
